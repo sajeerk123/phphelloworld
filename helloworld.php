d9b32b58f7cb78cee14c3da41ea2e8e113ddd154
sajeerk123 <sajeerkunnoth@gmail.com>
2016-10-19 19:54:14 +0530

    conflit remove

--- helloworld.php
+++ helloworld.php
@@ -5,8 +5,7 @@
 <?php
 echo "My firstiiiiiii PHP script!";
 echo "My first PHP script!";
-echo "My mjakating tinntfirst PHP script!";
-echo "this is test for php";
+echo "hiiiiiiiiii";
 ?> 
 
 </body>
