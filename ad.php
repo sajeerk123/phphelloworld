fd790d97195bcba085505d5cdd58ccad22b21b2d
sajeerk123 <sajeerkunnoth@gmail.com>
2016-10-21 17:13:12 +0530

    add ad.php

--- ad.php
+++ ad.php
@@ -73,8 +73,5 @@ echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
-echo "My firstiiiiiii PHPdajfgajlfbh script!";
-echo "My firstiiiiiii PHPdajfgajlfbh script!";
-echo "My firstiiiiiii PHPdajfgajlfbh script!";
 </body>
 </html>
