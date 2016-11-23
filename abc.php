c27bdfda98bae7e8df8ad3c745a60698db030665
sajeerk123 <sajeerkunnoth@gmail.com>
2016-11-23 12:55:15 +0530

    rmove line from 2 abc

--- abc.php
+++ abc.php
@@ -56,7 +56,5 @@ echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
 echo "My firstiiiiiii PHPdajfgajlfbh script!";
-echo "My firstiiiiiii PHPdajfgajlfbh script!";
-echo "My firstiiiiiii PHPdajfgajlfbh script!";
 </body>
 </html>
